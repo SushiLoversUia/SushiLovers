2a4ed4884401fae934c8bf8cc75f5d6a41e2163e
Arnaud Soler <arnaus17@student.uia.no>
2017-10-16 02:59:02 +0200

    adding database

--- server-proposal-Arnaud/server.js
+++ server-proposal-Arnaud/server.js
@@ -1,13 +1,28 @@
 const express = require('express')
 const app = express()
 
+/* Database connection */
+
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
+
+client.connect();
+
+/* Using public folder */
+
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function (req, res) {
+/* Example of use */
+
+app.get('/', function(req, res) {
     res.send('<h1> This is my proposal server for the project </h1>');
 });
 
-app.get('/about', function (req, res) {
+app.get('/about', function(req, res) {
     res.render('index.ejs');
 });
 
@@ -15,9 +30,62 @@ app.post('/another', function(req, res) {
     res.render('another.ejs');
 });
 
+/*  "/api/user"
+*   GET : find all users
+*   POST : create a new user 
+*/
+
+app.get('/api/user', function(req,res) {
+    client.query('SELECT idA FROM account;', (err, res) => {
+        if (err) throw err;
+        for (let row of res.rows) 
+        {
+            console.log(JSON.stringify(row));
+        }
+        client.end();
+    });
+});
+
+app.post('/api/user', function(req,res) {
+});
+
+/*  "/api/user/:id"
+*   GET : find a single user by ID
+*   PUT : update the user
+*   DELETE : delete a user by ID
+*/
+
+app.get('/api/user/:id', function(req,res) {
+});
+
+app.put('/api/user/:id', function(req,res) {
+});
+
+app.delete('/api/user/:id', function(req,res) {
+});
+
+/*  "/api/presentation/:id"
+*   GET : find a single presentation by ID
+*   PUT : update the presentation
+*   DELETE : delete a presentation by ID
+*/
+
+app.get('/api/presentation/:id', function(req,res) {
+});
+
+app.put('/api/presentation/:id', function(req,res) {
+});
+
+app.delete('/api/presentation/:id', function(req,res) {
+});
+
+/* Default adress */
+
 app.use(function(req, res, next) {
     res.redirect('/about');
-})
+});
+
+/* Listening PORT */
    
 app.listen(process.env.PORT || 8080, function () {
   console.log('My app listening on port 8080!');
