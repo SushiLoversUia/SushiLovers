6aa36b8408230839cd1f1b134f224a7902f8cfe5
Arnaud Soler <arnaus17@student.uia.no>
2017-11-22 10:48:06 +0100

    redesign of the database

--- users.js
+++ users.js
@@ -23,18 +23,17 @@ app.post('/', bodyParser, function(req,res) {
     var upload = JSON.parse(req.body);
     var userid = upload.loginname;
     var passw = bcrypt.hashSync(upload.password);
-    var fstname = upload.fstname;
-    var lstname = upload.lstname;
-    var bdate = "01-01-1990";
+    var fullname = upload.fullname;
+    var email = upload.email;
 
-    client.query('INSERT INTO account(userid,userpassw,fstname,lstname,birthday) VALUES(\'' + userid + '\',\'' + passw + '\',\'' + fstname + '\',\'' + lstname + '\',\'' + bdate + '\');', (err, resu) => {
+    client.query('INSERT INTO account(userid,userpassw,fullname,email) VALUES(\'' + userid + '\',\'' + passw + '\',\'' + fullname + '\',\'' + email + '\');', (err, resu) => {
         if (err) throw err;
 
         //create the token        
-        var payload = {loginname: userid, fstname: fstname, lstname: lstname};
+        var payload = {loginname: userid, fullname: fullname};
         var tok = jwt.sign(payload, secret, {expiresIn: "12h"});
         //send logininfo + token to the client
-        res.status(200).json({loginname: userid, fstname: fstname, lstname: lstname, token: tok}); 
+        res.status(200).json({loginname: userid, fullname: fullname, token: tok}); 
     });
 });
 
@@ -87,7 +86,7 @@ app.post('/auth', bodyParser, function(req,res) {
         else {
             var dbloginname = result.rows[0].userid;
             var dbpassword = result.rows[0].userpassw;
-            var dbfullname = result.rows[0].fstname; 
+            var dbfullname = result.rows[0].fullname; 
             //check if the password is correct
             var checkpwd = bcrypt.compareSync(password, dbpassword);
 
@@ -97,7 +96,7 @@ app.post('/auth', bodyParser, function(req,res) {
             }
         }
         //we have a valid user ‐> create the token
-        var payload = { loginname: dbloginname, fullname: dbpassword }; 
+        var payload = { loginname: dbloginname, fullname: dbfullname }; 
         var tok = jwt.sign(payload, secret, {expiresIn: "12h"});
         //send logininfo + token to the client
         res.status(200).json({loginname: dbloginname, fullname: dbfullname, token: tok});
