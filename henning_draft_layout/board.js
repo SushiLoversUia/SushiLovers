2bb98842b7ab73a957cc3f3ab627d8fd6627e5e0
Henning Holm <henningholm90@gmail.com>
2017-11-28 14:26:04 +0100

    Adding directly because of merging issues

---
+++ henning_draft_layout/board.js
@@ -0,0 +1,16 @@
+var wBoard = document.getElementById("whiteBoard");
+
+window.onload = function(){
+  // window.addEventListener('resize', init, false)
+}
+
+/*function init(){
+
+  var ccHeight =  document.getElementById("cContainer").clientHeight;
+  var ccWidth =  document.getElementById("cContainer").clientWidth;
+  wBoard.height =  (ccHeight-60)px;
+  wBoard.width =  (ccWidth-60)px;
+  console.log( ccHeight, ccWidth);
+  console.log( wBoard.height, wBoard.width);
+
+}*/
